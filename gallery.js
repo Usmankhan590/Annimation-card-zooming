2566be0dd52fb552fb464685c3fa6a816465570c
Usmankhan590 <usmanazizkhan5@gmail.com>
2023-01-18 05:21:07 +0500

    Annimation-card-zooming

---
+++ gallery.js
@@ -0,0 +1,8 @@
+// // selecting all required elements
+// const filteritem = document.quarysalector(".image");
+
+// window.onload = ()=>{once window loaded
+//     filteritem.onclick = ()=>{
+
+//     }
+// }
